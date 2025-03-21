2f5fb28c9a8295f2f42cbb3281052cd4cfe988e8
Егор <e.timoshek@mail.ru>
2025-03-21 12:30:31 +0300

    main added, many to many fix

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -34,7 +34,7 @@ public class Book {
 
     @ManyToMany // Указывает на связь многие-ко-многим с Author
     @JoinTable(
-            name = "bookauthors", // Имя связующей таблицы
+            name = "book_authors", // Имя связующей таблицы
             joinColumns = @JoinColumn(name = "book_id"), // Внешний ключ на таблицу Books
             inverseJoinColumns = @JoinColumn(name = "author_id") // Внешний ключ на таблицу Authors
     )
